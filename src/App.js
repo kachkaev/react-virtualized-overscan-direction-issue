49aeb7c7002533f61997ea4215083d333c47b229
Alexander Kachkaev <alexander.kachkaev@woodsbagot.com>
2017-01-31 18:04:28 +0000

    MWE with leaflet

--- src/App.js
+++ src/App.js
@@ -1,18 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Grid, AutoSizer } from 'react-virtualized'; 
+import { Map, TileLayer, Marker } from 'react-leaflet'; 
+
+import 'leaflet/dist/leaflet.css';
 import './App.css';
 
+const cellWidth = 500;
+const cellHeight = 500;
+
+const columnCount = 100;
+const rowCount = 100;
+
+const position = [51.505, -0.09];
+const Cell = ({style}) => (
+  <div style={style}>
+    <Map
+      zoomBehaviour={false}
+      zoomControl={false}
+      scrollWheelZoom={false}
+      boxZoom={false}
+      center={position}
+      zoom={13}
+      style={{
+        background: 'red',
+        position: 'absolute',
+        width: cellWidth - 20,
+        height: cellHeight - 20,
+      }}
+    >
+      <TileLayer
+        url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </Map>
+  </div>
+);
+
 class App extends Component {
+  cellRenderer = ({style, key, columnIndex, rowIndex}) => <Cell key={key} tileRandomness={key} style={style} />;
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AutoSizer>
+          {({width, height}) => (
+            <Grid
+              cellRenderer={this.cellRenderer}
+              columnWidth={cellWidth}
+              columnCount={columnCount}
+              height={height}
+              overscanColumnCount={2}
+              overscanRowCount={2}
+              // reverseOverscanColumnCount={2}
+              // reverseOverscanRowCount={2}
+              rowHeight={cellHeight}
+              rowCount={rowCount}
+              width={width}
+            />
+          )}
+      </AutoSizer>
       </div>
     );
   }
